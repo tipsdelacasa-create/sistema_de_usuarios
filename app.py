0f98fcbb848dff19acc33c19e0a44d5ff97e6adf
tipsdelacasa-create <tipsdelacasa@gmail.com>
2026-01-31 08:28:58 +0100

    commit inicial

--- app.py
+++
@@ -1,5 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-from sqlalchemy import Column, Integer, String
-from database import Base
-from fastapi import FastAPI
